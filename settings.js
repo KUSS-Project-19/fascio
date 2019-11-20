16915b53aac1fb55db78254a1b9e0381f7be4738
dlarudgus20 <dlarudgus20@naver.com>
2019-11-21 00:08:20 +0900

    settings.js validation

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const joi = require('@hapi/joi')
 
 const confFilePath = path.join(__dirname, 'etc/settings.json')
 
@@ -10,6 +11,23 @@ module.exports.get = function() {
         return settings
     }
 
-    settings = JSON.parse(fs.readFileSync(confFilePath))
-    return settings
+    try {
+        settings = JSON.parse(fs.readFileSync(confFilePath))
+
+        const schema = joi.object({
+            dvid: joi.number().required().strict().integer().min(1),
+            pass: joi.string().required(),
+            baseUrl: joi.string().required().uri()
+        })
+        const result = schema.validate(settings)
+        if (result.error !== undefined) {
+            throw result.error
+        }
+
+        return settings
+    }
+    catch (e) {
+        console.log(`etc/settings.json is invalid: ${e.message}`)
+        process.exit(1)
+    }
 }
